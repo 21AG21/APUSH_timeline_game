13ecfe8cbc8b919260f5c88d7b0eaf3af89decb8
Arjun <arjungandhi01@gmail.com>
2026-06-06 16:31:51 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/APUSH_timeline_game/', // <-- ADD THIS EXACT LINE
   plugins: [react()],
   server: {
     allowedHosts: true
